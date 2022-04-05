713ee25e9f5185f8dc456979b547426f9278cacb
SomexJames <93785036+SomexJames@users.noreply.github.com>
2022-04-05 10:01:01 -0500

    Update FIN6392G3Token.sol

--- FIN6392G3Token.sol
+++ FIN6392G3Token.sol
@@ -1,7 +1,8 @@
 pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-contract G3Tk {
-
-    
+contract TokenName is ERC20 {
+    constructor() ERC20("TokenName", "SYMBOL") {
+        _mint(msg.sender, 10000000 * 10 ** 18);
+    }
 }
