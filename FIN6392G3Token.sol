8fe41a37fbbde2f5badc759e830cc1d1b90d2a59
SomexJames <93785036+SomexJames@users.noreply.github.com>
2022-04-07 22:13:06 -0500

    Updated token
    Need to debug "deposit()" function

--- FIN6392G3Token.sol
+++ FIN6392G3Token.sol
@@ -1,8 +1,30 @@
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenName is ERC20 {
-    constructor() ERC20("TokenName", "SYMBOL") {
-        _mint(msg.sender, 10000000 * 10 ** 18);
+    address public owner;
+    uint256 public balance;
+
+    event TransferReceived(address _from, uint _amount);
+
+    constructor(uint256 initialSupply) ERC20("BVJMToken", "BVJM") {
+        _mint(address(this), initialSupply * 10 ** 18);
+        owner = msg.sender;
     }
+    
+    receive() external payable {
+        balance += msg.value;
+        emit TransferReceived(msg.sender, msg.value);
+    }
+
+    function exchange() public payable {
+        (bool sent, bytes memory data) = address(this).call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+        _mint(msg.sender, msg.value * 500);
+    }
+
+    function deposit(uint256 amount, TokenName token) public payable {
+        token.transferFrom(msg.sender, address(this), amount);
+    }
+
 }
