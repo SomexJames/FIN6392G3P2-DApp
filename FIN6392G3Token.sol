7010db2789d956e974930494ab8db91dacf3696c
SomexJames <93785036+SomexJames@users.noreply.github.com>
2022-04-09 14:30:43 -0500

    Finished all base logic of smart contract
    Ended up creating a new instance of the "Bank" contract in the token contract so that the token contract had control over the bank functions.

--- FIN6392G3Token.sol
+++ FIN6392G3Token.sol
@@ -1,31 +1,66 @@
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokenName is ERC20 {
+
+
+contract G3Token is ERC20 {
+    
+    Bank public bank;
     address public owner;
-    uint256 public balance;
+    uint256 public etherBalance;
 
     event TransferReceived(address _from, uint _amount);
     event TransferSent(address _from, address _destAddr, uint _amount);
 
 // Constructor mints initial supply to our Bank contract (we'll add as param when we deploy)
-    constructor(uint256 initialSupply, address payable receiver) ERC20("BVJMToken", "BVJM") {
-        _mint(receiver, initialSupply * 10 ** 18);
+    constructor(uint256 initialSupply) ERC20("BVJMToken", "BVJM") {
+        bank = new Bank();
+        _mint(payable(address(bank)), initialSupply * 10 ** 18);
         owner = msg.sender;
     }
     
 // Receives and stores ether payment
     receive() external payable {
-        balance += msg.value;
+        etherBalance += msg.value;
         emit TransferReceived(msg.sender, msg.value);
     }
 
-// Stores the ether through receive() and mints 5000 tokens per ether
-    function exchange() public payable {
+// Stores the ether through receive()
+    function exchange() external payable {
         (bool sent, bytes memory data) = address(this).call{value: msg.value}("");
         require(sent, "Failed to send Ether");
-        _mint(msg.sender, msg.value * 5000);
+        bank.payout(ERC20(address(this)), msg.value * 5000, payable(msg.sender));
     }
 
+    function sendPayout() public payable {
+        bank.payout(ERC20(address(this)), msg.value, payable(msg.sender));
+    }
+
+
+}
+
+
+contract Bank{
+    address public owner;
+    uint public tokenBalance;
+
+    event TransferReceived(address _from, uint _amount);
+    event TransferSent(address _from, address _destAddr, uint _amount);
+
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    receive() external payable {
+        tokenBalance += msg.value;
+        emit TransferReceived(msg.sender, msg.value);
+    }
+
+    function payout(ERC20 token, uint amount, address payable to) public {
+        uint256 erc20balance = token.balanceOf(address(this));
+        require(amount <= erc20balance, "Balance is too low");
+        token.transfer(to, amount);
+        emit TransferSent(msg.sender, to, amount);
+    }
 
 }
