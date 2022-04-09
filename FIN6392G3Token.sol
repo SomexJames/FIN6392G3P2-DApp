7325f6503e25d20554f611ee2280c63ee4aa8449
SomexJames <93785036+SomexJames@users.noreply.github.com>
2022-04-08 21:23:54 -0500

    Main token functions
    Note: This is when we decided to create a new contract to handle transfer of tokens

--- FIN6392G3Token.sol
+++ FIN6392G3Token.sol
@@ -6,25 +6,26 @@ contract TokenName is ERC20 {
     uint256 public balance;
 
     event TransferReceived(address _from, uint _amount);
+    event TransferSent(address _from, address _destAddr, uint _amount);
 
-    constructor(uint256 initialSupply) ERC20("BVJMToken", "BVJM") {
-        _mint(address(this), initialSupply * 10 ** 18);
+// Constructor mints initial supply to our Bank contract (we'll add as param when we deploy)
+    constructor(uint256 initialSupply, address payable receiver) ERC20("BVJMToken", "BVJM") {
+        _mint(receiver, initialSupply * 10 ** 18);
         owner = msg.sender;
     }
     
+// Receives and stores ether payment
     receive() external payable {
         balance += msg.value;
         emit TransferReceived(msg.sender, msg.value);
     }
 
+// Stores the ether through receive() and mints 5000 tokens per ether
     function exchange() public payable {
         (bool sent, bytes memory data) = address(this).call{value: msg.value}("");
         require(sent, "Failed to send Ether");
-        _mint(msg.sender, msg.value * 500);
+        _mint(msg.sender, msg.value * 5000);
     }
 
-    function deposit(uint256 amount, TokenName token) public payable {
-        token.transferFrom(msg.sender, address(this), amount);
-    }
 
 }
